f358c5fb11fe1ba6fa8592bd86704794d4c9e9ff
itspitso <147466450+itspitso@users.noreply.github.com>
2025-02-16 21:50:32 +0200

    Changed variable name

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date')
+    list_display = ('title', 'author', 'publication_year')
     search_fields = ('title', 'author')
-    list_filter = ('title', 'author', 'published_date')
+    list_filter = ('title', 'author', 'publication_year')
     
 admin.site.register(Book, BookAdmin)
