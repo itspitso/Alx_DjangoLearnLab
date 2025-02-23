a375ea0d6a8fc57b27742dad6c5f7961f5ffe43c
itspitso <147466450+itspitso@users.noreply.github.com>
2025-02-23 20:17:01 +0200

    Fixed queries in query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,3 +1,4 @@
 Book.objects.filter(author="name")
-Library.objects.get(name=library_name, books.all())
+Library.objects.get(name=library_name)
+Library.objects.get(books.all())
 Librarian.objects.get(Library="library")
