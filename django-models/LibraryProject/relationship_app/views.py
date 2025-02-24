e5cef81eaff52aed530882b043c727bae038fd2f
itspitso <147466450+itspitso@users.noreply.github.com>
2025-02-24 09:14:16 +0200

    Added LibraryViewDetail

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,7 +13,7 @@ def list_books(request):
     }
     return(request, "relationship_app/list_books.html", context)
 
-class LibraryDetail(DetailView):
+class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = 'library'
