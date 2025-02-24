9749e330a71baf36a8f9f86ebd5c3f1f0b33c841
itspitso <147466450+itspitso@users.noreply.github.com>
2025-02-24 06:34:59 +0200

    Implementing views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views.generic.list import DetailView
+from .models import Book, Library
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.values('title', 'author')
+    context = {
+        'books': books
+    }
+    return(request, "list_books.html", context)
+
+class LibraryDetail(DetailView):
+    model = Library
+    template_name = "library_detail.html"
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
