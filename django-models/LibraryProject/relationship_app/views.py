47e9f9453080a6fab5790e88c5de6a966d69f554
itspitso <147466450+itspitso@users.noreply.github.com>
2025-02-24 09:09:29 +0200

    Fixed path to templates in views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -14,7 +14,7 @@ def list_books(request):
 
 class LibraryDetail(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
