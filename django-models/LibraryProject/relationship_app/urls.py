0d31bae84d48a62e38853fa7ec5b2953dee0d206
itspitso <147466450+itspitso@users.noreply.github.com>
2025-02-25 08:48:15 +0200

    Updating project directory

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
     path('books/', list_books, name="list_books"),
     path('library/<str:name>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register, name='register')
 ]
