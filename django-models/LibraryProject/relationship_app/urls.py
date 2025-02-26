510e8dd58344863869f379af5c28a7d8fad0794e
itspitso <147466450+itspitso@users.noreply.github.com>
2025-02-26 06:03:34 +0200

    Added templates to login and logout views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('books/', list_books, name="list_books"),
     path('library/<str:name>/', LibraryDetailView.as_view(), name='library_detail'),
     path('register/', register, name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
